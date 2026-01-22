2b2b6db1886900dc47b0c604f3295639e48f9ec9
mark3473 <mark3473@naver.com>
2026-01-22 13:12:31 +0900

    init

---
+++ backend/src/main/java/com/aivle/cosy/CosyApplication.java
@@ -0,0 +1,13 @@
+package com.aivle.cosy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/aivle/cosy/CosyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aivle.cosy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
