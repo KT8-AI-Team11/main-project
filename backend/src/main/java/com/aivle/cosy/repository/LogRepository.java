091063e0d544ad7ec7c33c77f05e1536451422a1
anna <18yk04@gmail.com>
2026-01-30 11:22:53 +0900

    chore: 보안을 위해 application.yaml 기록 삭제 및 제외

---
+++ backend/src/main/java/com/aivle/cosy/repository/LogRepository.java
@@ -0,0 +1,12 @@
+//package com.aivle.cosy.repository;
+//
+//import com.aivle.cosy.domain.Log;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//public interface LogRepository extends JpaRepository<Log, Long> {
+//    Optional<Log> findByProductId();
+//    List<Log> findByCountry();
+//}
