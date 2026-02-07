cf8dd355b8252801e19d0286f3bee8ec8c8861c8
mark3473 <mark3473@naver.com>
2026-02-07 18:25:03 +0900

    add: auth api 관련 로그 추가

--- backend/src/main/java/com/aivle/cosy/dto/LoginRequest.java
+++ backend/src/main/java/com/aivle/cosy/dto/LoginRequest.java
@@ -1,15 +1,13 @@
 package com.aivle.cosy.dto;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class
 LoginRequest {
     private String email;
