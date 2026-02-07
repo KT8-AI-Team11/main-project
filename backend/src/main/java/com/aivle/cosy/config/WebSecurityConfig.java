3fd9b435d8f60c9e87b6222cdb7c87fada616033
mark3473 <mark3473@naver.com>
2026-02-07 15:40:33 +0900

    allowed 출처 추가

--- backend/src/main/java/com/aivle/cosy/config/WebSecurityConfig.java
+++ backend/src/main/java/com/aivle/cosy/config/WebSecurityConfig.java
@@ -55,6 +55,7 @@ public class WebSecurityConfig {
        config.addAllowedOrigin("http://localhost:5173");
        config.addAllowedOrigin("http://127.0.0.1:5500");
        config.addAllowedOrigin("https://dd1luume97j85.cloudfront.net");
+       config.addAllowedOrigin("http://cosy-alb-1486968001.ap-northeast-2.elb.amazonaws.com");
 
        // 2. 허용할 HTTP 메서드 (GET, POST, PUT, DELETE 등)
        config.addAllowedMethod("*");
