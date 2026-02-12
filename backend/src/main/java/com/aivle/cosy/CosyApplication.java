fe77c9940d668fff39ea41150026c4f6c8026068
Jeonyg <jyg11240518@gmail.com>
2026-02-12 16:52:56 +0900

    이메일을 입력하세요 문구 변경

--- backend/src/main/java/com/aivle/cosy/CosyApplication.java
+++ backend/src/main/java/com/aivle/cosy/CosyApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CosyApplication {
+public class 	CosyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CosyApplication.class, args);
